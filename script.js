8620c6ad6f4201ca521a105089cef27593e54c6f
Restlessking <vigneshjayachandiran95@gmail.com>
2024-08-30 10:12:38 +0530

    updated

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 //!Creating a HTML Element
 
-const heading=document.createElement("h1")
-const div=document.createElement("div")
-const div1=document.createElement("div")
-const div2=document.createElement("div")
-const btn1=document.createElement("button")
-const btn2=document.createElement("button")
-const btn3=document.createElement("button")
-const btn4=document.createElement("button")
+const heading = document.createElement("h1");
+const div = document.createElement("div");
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const btn1 = document.createElement("button");
+const btn2 = document.createElement("button");
+const btn3 = document.createElement("button");
+const btn4 = document.createElement("button");
 
 //!Adding the Content
 
-heading.innerHTML="Event Listener"
-btn1.innerHTML="Red"
-btn2.innerHTML="Green"
-btn3.innerHTML="Blue"
-btn4.innerHTML="Reset"
+heading.innerHTML = "Event Listener";
+btn1.innerHTML = "Red";
+btn2.innerHTML = "Green";
+btn3.innerHTML = "Blue";
+btn4.innerHTML = "Reset";
 
 //!Adding Attributes
 
-heading.setAttribute("id","head")
-div.setAttribute("id","container")
-div1.setAttribute("id","circle")
-div2.setAttribute("id","box")
-btn1.setAttribute("id","red")
-btn1.setAttribute("class","btn")
-btn2.setAttribute("id","green")
-btn2.setAttribute("class","btn")
-btn3.setAttribute("id","blue")
-btn3.setAttribute("class","btn")
-btn4.setAttribute("id","reset")
-btn4.setAttribute("class","btn")
+heading.setAttribute("id", "head");
+div.setAttribute("id", "container");
+div1.setAttribute("id", "circle");
+div2.setAttribute("id", "box");
+btn1.setAttribute("id", "red");
+btn1.setAttribute("class", "btn");
+btn2.setAttribute("id", "green");
+btn2.setAttribute("class", "btn");
+btn3.setAttribute("id", "blue");
+btn3.setAttribute("class", "btn");
+btn4.setAttribute("id", "reset");
+btn4.setAttribute("class", "btn");
 
 //!Appending elements
 
-div1.appendChild(div2)
-div.appendChild(div1)
-div.append(btn1,btn2,btn3,btn4)
-document.body.append(heading,div)
-
+div1.appendChild(div2);
+div.appendChild(div1);
+div.append(btn1, btn2, btn3, btn4);
+document.body.append(heading, div);
 
 //!Javascript function
 
@@ -54,8 +53,6 @@ head.addEventListener("mousemove", () => {
   head.style.backgroundColor = "#ffffff";
   head.style.color = "#000000";
 });
-
-
 red.addEventListener("mousemove", () => {
   container.style.backgroundColor = "red";
   box.style.backgroundColor = "red";
